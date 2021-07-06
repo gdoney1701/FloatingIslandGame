5e1a148e187a85d80f299e4258824a4a11d2d42e
Grant <gdoney1701@gmail.com>
2021-07-06 18:01:06 -0400

    Added code to stop platforms for clipping

--- FI_GameClient/Assets/SampleSceneAssets/Scripts/IslandGeneration.cs
+++ FI_GameClient/Assets/SampleSceneAssets/Scripts/IslandGeneration.cs
@@ -9,6 +9,8 @@ public class IslandGeneration : MonoBehaviour
     public int islandNumber = 10;
     public GameObject islandPrefab;
     public GameObject islandParent;
+    public int failureThreshold = 10;
+    public int platformMaxSize = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,7 @@ public class IslandGeneration : MonoBehaviour
         List<Vector3> pointStart = new List<Vector3>();
         float minRad = radius - shellThickness;
         int i = 0;
+        int failCounter = 0;
         while( i < islandNumber)
         {
             Vector3 newCoords = new Vector3(
@@ -26,18 +29,29 @@ public class IslandGeneration : MonoBehaviour
                 Mathf.Pow(newCoords.x, 2) + Mathf.Pow(newCoords.y, 2) + Mathf.Pow(newCoords.z, 2));
             if (vectorCheck >= minRad && vectorCheck <= radius)
             {
+                Collider[] areaCheck = Physics.OverlapSphere(newCoords, platformMaxSize);
+                if (areaCheck.Length > 0)
+                {
+                    failCounter++;
+                    if (failCounter > 0)
+                    {
+                        failCounter = 0;
+                        i++;
+                    }
+                    continue;
+                }
                 pointStart.Add(newCoords);
                 i++;
+                failCounter = 0;
                 continue;
             }
-            continue;
-
         }
         foreach(Vector3 entry in pointStart)
         {
             GameObject newIsland = Instantiate(islandPrefab, entry, new Quaternion(0,0,0,0));
-            newIsland.transform.parent = newIsland.transform;
+            newIsland.transform.SetParent(islandParent.transform);
         }
+        islandParent.name += pointStart.Count.ToString();
     }
 
     // Update is called once per frame
