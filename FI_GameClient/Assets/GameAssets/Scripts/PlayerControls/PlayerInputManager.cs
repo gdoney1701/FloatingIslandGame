25d0bb1a8a82096c49c557d64ccd2ee3a315f5e0
Grant <gdoney1701@gmail.com>
2021-07-07 15:07:41 -0400

    Added simple character controller

---
+++ FI_GameClient/Assets/GameAssets/Scripts/PlayerControls/PlayerInputManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    private FPS_ControlActions controls;
+    public Vector2 move;
+    public bool jumped = false;
+
+    public static PlayerInputManager _instance;
+
+    public static PlayerInputManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        controls = new FPS_ControlActions();
+
+        controls.Keyboard.Movement.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.Keyboard.Movement.canceled += ctx => move = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        controls.Keyboard.Enable();
+    }
+    private void OnDisable()
+    {
+        controls.Keyboard.Disable();
+    }
+
+    public bool PlayerJumpedThisFrame()
+    {
+        return controls.Keyboard.Jump.triggered;
+    }
+}
